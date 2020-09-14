c2e440a474b900b58bb29160a231c00f52c76db0
jerome0caruso <jerome@myjavascriptprojects.com>
2020-09-14 10:40:23 -0500

    adding css grid

---
+++ food/src/home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+
+function Home() {
+    return(
+        <div className="main-container">
+            <div className="nav-bar">
+                <NavBar />
+            </div>
+            <div className="main-photo">
+                <photo />
+            </div>
+            <div className="nav-bar">
+                <NavBar />
+            </div>
+            <div className="nav-bar">
+                <NavBar />
+            </div>
+            <div className="nav-bar">
+                <NavBar />
+            </div>
+        </div>
+
+
+
+
+    )
+}
+
+export default Home;
