d3fe68fb80fda895cade12aae77e8e9f6ddca545
jerome0caruso <jerome0caruso@myjavascriptprojects.com>
2020-09-16 10:50:29 -0500

    Updates

---
+++ precache-manifest.b1f4efbf1b31fe25e165b8528e765b05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc1485408b974ce036b9997d3d5ab2e",
+    "url": "/food/index.html"
+  },
+  {
+    "revision": "49cc9149ba2fad01990c",
+    "url": "/food/static/css/main.a3619f54.chunk.css"
+  },
+  {
+    "revision": "a833d4997ccbf3c0c3ba",
+    "url": "/food/static/js/2.7d4e5656.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/food/static/js/2.7d4e5656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49cc9149ba2fad01990c",
+    "url": "/food/static/js/main.f90b8234.chunk.js"
+  },
+  {
+    "revision": "b38c118dfd6728e87d2d",
+    "url": "/food/static/js/runtime-main.1f1ea413.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/food/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "037a92e0aabb69868e9088ac06197fb9",
+    "url": "/food/static/media/testpicfood.037a92e0.jpg"
+  }
+]);
